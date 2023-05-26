78a4d635a17263bdf64eb13a0d358e823544f836
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-05-26 20:11:45 -0300

    Requisito 2: Corrigna a função getById da camada service de sales

--- backend/src/services/sales.service.js
+++ backend/src/services/sales.service.js
@@ -12,7 +12,7 @@ const getAll = async () => {
 const getById = async (id) => {
   try {
     const result = await salesModel.getById(id);
-    if (!result) return { type: 404, message: 'Sale not found' };
+    if (result.length === 0) return { type: 404, message: 'Sale not found' };
     return { type: null, message: result };
   } catch (e) {
     return { type: 500, message: 'Internal server error' };
