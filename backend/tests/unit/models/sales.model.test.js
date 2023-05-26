bb1778c0e3ff2e65c219a3e88971d3861082b0e1
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-05-26 18:24:11 -0300

    Requisito 2: Testes da função getAll da camada model de sales

---
+++ backend/tests/unit/models/sales.model.test.js
@@ -0,0 +1,15 @@
+const { expect } = require('chai');
+const { describe, it } = require('mocha');
+const sinon = require('sinon');
+const connection = require('../../../src/models/connection');
+const { sales } = require('./mocks/sales.model.mocks');
+const { salesModel } = require('../../../src/models');
+
+describe('Testes de sales na camada model', function () {
+  afterEach(sinon.stub);
+
+  it('getAll', async function () {
+    sinon.stub(connection, 'execute').resolves(sales);
+    expect(await salesModel.getAll()).to.be.deep.equal(sales);
+  });
+});
