56f48c36a8495f76885c3634e98223a9394b2601
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-05-27 22:40:04 -0300

    Requisito 6: Remove erros de lint

--- backend/src/middlewares/index.js
+++ backend/src/middlewares/index.js
@@ -1,4 +1,3 @@
 const productsMiddlewares = require('./products.middlewares');
-const salesMiddlewares = require('./sales.middlewares');
 
-module.export = { productsMiddlewares, salesMiddlewares };
+module.export = { productsMiddlewares };
